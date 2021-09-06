5396a5c792bce2e446d44bfdae364e5719465801
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-06 16:03:48 +0700

    Create a new Kotlin file

---
+++ app/src/main/java/com/rujirakongsomran/jc_signupwithgooglebutton/GoogleButton.kt
@@ -0,0 +1,21 @@
+package com.rujirakongsomran.jc_signupwithgooglebutton
+
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@ExperimentalMaterialApi
+@Composable
+fun GoogleButton() {
+    Surface(onClick = {}){
+
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+@Preview
+private fun GoogleButtonPreview() {
+    GoogleButton()
+}
