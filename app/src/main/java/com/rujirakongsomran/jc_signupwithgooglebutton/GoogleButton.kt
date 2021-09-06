3baca94dec7d9c452fae60d1131f2dd1c2bd78ac
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-06 16:09:45 +0700

    Clicked State

--- app/src/main/java/com/rujirakongsomran/jc_signupwithgooglebutton/GoogleButton.kt
+++ app/src/main/java/com/rujirakongsomran/jc_signupwithgooglebutton/GoogleButton.kt
@@ -3,12 +3,17 @@ package com.rujirakongsomran.jc_signupwithgooglebutton
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import  androidx.compose.runtime.*
 
 @ExperimentalMaterialApi
 @Composable
 fun GoogleButton() {
-    Surface(onClick = {}){
+    var clicked by remember { mutableStateOf(false) }
+    Surface(onClick = { clicked = !clicked }
+    ) {
 
     }
 }
