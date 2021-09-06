05499e19716a3150a946388d796ba89cee4bc9d2
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-06 22:34:17 +0700

    Customize Parameters

--- app/src/main/java/com/rujirakongsomran/jc_signupwithgooglebutton/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_signupwithgooglebutton/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.rujirakongsomran.jc_signupwithgooglebutton
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,7 +26,13 @@ class MainActivity : ComponentActivity() {
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    GoogleButton()
+                    GoogleButton(
+                        text = "Sign Up with Google",
+                        loadingText = "Creating Account...",
+                        onClicked = {
+                            Log.d("googleButton", "Clicked!")
+                        }
+                    )
                 }
             }
         }
